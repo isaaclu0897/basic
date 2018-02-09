99aa2939037f27397af4c91f475ce82bd0a1e1af
wei <t104306033@ntut.org.tw>
2018-02-09 23:30:00 +0800

    introduce and show hello world

---
+++ basic/python&English/01_hello_world.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 10 00:52:08 2018
+
+@author: wei
+"""
+
+'''
+Python is a clean and powerful programing language,
+
+you can use it to do many thing,
+
+I also analyze data, and write the web crawler,
+
+first, you need to show "Hello World" in console or commander,
+
+they can help you to learn programe fast,
+
+please believe it
+'''
+
+print("Hello World")
+
+# this is very easy to print, you don't need to add semicolon(分號) like C, Java etc.
+
+# Hello World between the two double quote, let the Python know this is sting
